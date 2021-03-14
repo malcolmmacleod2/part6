bfe28054e2e8077f523b9e0ac40871fd5fd9d441
Malcolm Macleod <malcolm@macleodhome.com>
2021-03-14 14:14:04 +0000

    6.19 - Use connect to wire up state for notification

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+
+
   const style = {
     border: 'solid',
     padding: 10,
@@ -10,9 +11,17 @@ const Notification = () => {
   }
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
