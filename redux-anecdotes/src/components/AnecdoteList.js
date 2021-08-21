31f3f28763181a003dc728c6d1f5a420ecb5d476
Malcolm Macleod <malcolm@macleodhome.com>
2021-08-21 23:13:47 +0100

    6.21 - ensure 5 second notification if last

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,38 +1,36 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { voteForAnecdote } from '../reducers/anecdoteReducer'
-import { createNotification, removeNotification } from '../reducers/notificationReducer'
+import { createNotification } from '../reducers/notificationReducer'
 
 const Anecdotes = () => {
   const dispatch = useDispatch()
 
-  const anecdotes = useSelector(({filter, anecdotes}) => {
-    if ( filter === '' ) {
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === '') {
       return anecdotes.sort((a, b) => b.votes - a.votes)
     }
-    return anecdotes.filter(a => a.content.includes(filter)).sort((a, b) => b.votes - a.votes)
+    return anecdotes
+      .filter((a) => a.content.includes(filter))
+      .sort((a, b) => b.votes - a.votes)
   })
 
   const vote = (anecdote) => {
     dispatch(voteForAnecdote(anecdote))
     dispatch(createNotification(`You voted for ${anecdote.content}`, 5))
-    setTimeout(() => dispatch(removeNotification()), 5000)
-
   }
 
-  return(
+  return (
     <div>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,34 +1,42 @@
 const initialState = 'hello'
+let timeoutId = null
 
 export const removeNotification = () => {
   return {
-      type: 'REMOVE'
-    }
+    type: 'REMOVE',
   }
+}
 
 export const createNotification = (message, seconds) => {
-  return dispatch =>  {
-      dispatch({
-        type: 'NOTIFY',
-        data: message
-      })
-      setTimeout(() => dispatch(removeNotification()), seconds * 1000)
+  return (dispatch) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
     }
+
+    dispatch({
+      type: 'NOTIFY',
+      data: message,
+    })
+    setTimeout(() => {
+      timeoutId = setTimeout(
+        () => dispatch(removeNotification()),
+        seconds * 1000
+      )
+    })
   }
-
-
+}
 
 const notificationReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  switch(action.type) {
+  switch (action.type) {
     case 'NOTIFY':
-        state = action.data
-        return state
+      state = action.data
+      return state
     case 'REMOVE':
-        state = null
-        return state
+      state = null
+      return state
     default:
       return state
   }
